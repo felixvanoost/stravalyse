f8539323bffc0d070ecb7fb605053dee32633597
Felix van Oost <felix.vanoost@live.com>
2019-06-11 19:13:40 -0400

    [Credentials] Add credentials text file template

--- StravaHeatmap.py
+++ StravaHeatmap.py
@@ -1,6 +1,20 @@
-# Strava Heatmap Tool
-#
-# This tool generates a heatmap of activities from Strava using the HERE XYZ mapping service.
+"""StravaHeatmap.py
+
+Felix van Oost 2019
+
+This tool generates a heatmap of Strava activities using the HERE XYZ mapping service.
+"""
+import os
+import strava_auth
+import sys
+
+# Add the folder containing the Strava Swagger API to the system path
+sys.path.append(os.path.abspath('API'))
+
+import swagger_client
+from swagger_client.rest import ApiException
 
 # Main module
 if __name__ == "__main__":
+
+    strava_auth.authenticate()
