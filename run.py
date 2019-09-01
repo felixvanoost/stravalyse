b1038e77a0b3ed0a115e39b9783c815a01936d44
Felix van Oost <felix.vanoost@live.com>
2019-08-31 23:33:39 -0400

    [Cleanup] Refactor run.py according to PEP 8
    - Rewrite method and module docstrings
    - Reorder imports
    - Update general spacing and formatting

--- run.py
+++ run.py
@@ -10,8 +10,12 @@ import analysis
 import geo
 import strava_data
 
-# Main module
+
 if __name__ == "__main__":
+    """
+    Main method for the Strava Analysis Tool.
+    """
+    
     print('Strava Analysis Tool')
     print('Felix van Oost 2019')
     print()
@@ -19,12 +23,12 @@ if __name__ == "__main__":
     # Get a list of detailed activity data for all Strava activities
     activity_data = strava_data.get_activity_data()
 
-    # Create a pandas DataFrame from the activities list
+    # Create a pandas DataFrame from the activity data
     activity_dataframe = analysis.create_activity_dataframe(activity_data)
 
     # Display summary and commute statistics
     analysis.display_summary_statistics(activity_dataframe)
     analysis.display_commute_statistics(activity_dataframe)
 
-    # Create a GeoJSON file from the geographic activity data
+    # Export a GeoJSON file of geospatial data from all activities
     geo.export_geo_data_file(activity_dataframe)
