d3432ba9f88ce66aa5a34c75d96a8a27f679bcf6
Felix van Oost <felix.vanoost@live.com>
2024-03-19 03:39:15 +0000

    Create Python project framework

---
+++ stravalyse/stravalyse.py
@@ -0,0 +1,18 @@
+#! /usr/bin/python
+
+""" stravalyse.py
+
+A tool to visualise and analyse Strava activities.
+"""
+
+
+def main() -> None:
+    """
+    Main method for stravalyse.
+    """
+
+    print('stravalyse')
+
+
+if __name__ == '__main__':
+    main()
